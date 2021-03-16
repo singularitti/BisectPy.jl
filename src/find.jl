49752a3af1bc1cf126474c1d45c38b1885cd21a1
singularitti <singularitti@outlook.com>
2021-03-16 01:04:21 -0400

    Update docs

--- src/find.jl
+++ src/find.jl
@@ -1,28 +1,48 @@
-"Locate the leftmost value exactly equal to x."
+"""
+    index(a, x)
+
+Locate the leftmost value exactly equal to `x` in `a`.
+"""
 function index(a, x)
     i = bisect_left(a, x)
     return i != length(a) + 1 && a[i] == x ? i : nothing
 end
 
-"Find rightmost value less than x."
+"""
+    find_lt(a, x)
+
+Find rightmost value less than `x` in `a`.
+"""
 function find_lt(a, x)
     i = bisect_left(a, x)
     return i > 1 ? a[i-1] : nothing
 end
 
-"Find rightmost value less than or equal to x."
+"""
+    find_le(a, x)
+
+Find rightmost value less than or equal to `x` in `a`.
+"""
 function find_le(a, x)
     i = bisect_right(a, x)
     return i > 1 ? a[i-1] : nothing
 end
 
-"Find leftmost value greater than x."
+"""
+    find_gt(a, x)
+
+Find leftmost value greater than `x` in `a`.
+"""
 function find_gt(a, x)
     i = bisect_right(a, x)
     return i != length(a) + 1 ? a[i] : nothing
 end
 
-"Find leftmost item greater than or equal to x."
+"""
+    find_ge(a, x)
+
+Find leftmost item greater than or equal to `x` in `a`.
+"""
 function find_ge(a, x)
     i = bisect_left(a, x)
     return i != length(a) + 1 ? a[i] : nothing
