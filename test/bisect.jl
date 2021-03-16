b787e05b04e7dcd941c4715e6f464dc61d38d9c0
singularitti <singularitti@outlook.com>
2021-03-15 23:38:32 -0400

    Add some tests

--- test/bisect.jl
+++ test/bisect.jl
@@ -8,8 +8,30 @@ using BisectPy
 
 @test bisect_left([1, 2, 3, 3, 3, 5], 3) == 3
 
+@test bisect_left([1, 2, 3, 4, 5], 0) == 1
+
 @test bisect_right([1, 2, 3, 4, 5], 3.5) == 4
 
 @test bisect_right([1, 2, 3, 4, 5], 2) == 3
 
 @test bisect_right([1, 2, 3, 3, 3, 5], 3) == 6
+
+@test bisect_right([1, 2, 3.0, 4, 5], 6) == 6
+
+@testset "`bisect_right`" begin
+    function grade(score, breakpoints = [60, 70, 80, 90], grades = "FDCBA")
+        i = bisect(breakpoints, score)
+        return grades[i]
+    end
+    @test [grade(score) for score in [33, 99, 77, 70, 89, 90, 100]] == ['F', 'A', 'C', 'C', 'B', 'A', 'A']
+end
+
+@testset "`bisect_left`" begin
+    data = [("red", 5), ("blue", 1), ("yellow", 8), ("black", 0)]
+    sort!(data)
+    keys = [r[2] for r in sort(data)]
+    @test data[bisect_left(keys, 0)] == ("black", 0)
+    @test data[bisect_left(keys, 1)] == ("blue", 1)
+    @test data[bisect_left(keys, 5)] == ("red", 5)
+    @test data[bisect_left(keys, 8)] == ("yellow", 8)
+end
